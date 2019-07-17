9be9ed74d10d3be3f49095dd2dd0372ebd9ba849
okylitski <okylitski@gmail.com>
2019-07-17 09:32:30 +0200

    e

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,11 @@
 var elem = document.querySelector('.dot_point');
-console.log(elem)
+var dot = document.querySelector('.dot_1');
+elem.onclick = function(){
+dot.style.margin = "0 0 -60px -22px"
+};
+
+
+
 
 
 
