72fff2fd75c10262e12875f1c628def462cc095f
okylitski <okylitski@gmail.com>
2019-07-19 23:11:05 +0200

    add new function

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,14 @@ var navigation_line = document.querySelector('.navigation_line');
 var products = document.querySelector('.products');
 var home = document.querySelector('.home');
 var about = document.querySelector('.about');
+var language = document.querySelector('.language');
+var choice_language = document.querySelector('.choice_language');
+var information = document.querySelector('.information');
+var information_window = document.querySelector('.information_window');
+var menu = document.querySelector('.menu');
+var choice_menu = document.querySelector('.choice_menu');
+var mainscreen_wooden = document.querySelector('.mainscreen_wooden');
+
 dot_1.onclick = function () {
     elem.style.left = "-22px";
     elem.style.top = "208px";
@@ -37,10 +45,30 @@ home.onclick = function () {
     navigation_line.style.width = "50px";
 };
 about.onclick = function () {
-    navigation_line.style.transform = "translate(1117px,10px)"
+    navigation_line.style.transform = "translate(1115px,10px)"
     navigation_line.style.width = "60px";
 };
-
+choice_language.onclick = function () {
+    choice_language.classList.remove("show")
+};
+language.onclick = function () {
+    choice_language.classList.toggle("show")
+    setTimeout(function () {
+        choice_language.classList.remove("show");
+    }, 10000);
+};
+information.onclick = function () {
+    information_window.classList.toggle("show")
+    setTimeout(function () {
+        information_window.classList.remove("show");
+    }, 10000);
+};
+menu.onclick = function () {
+    choice_menu.classList.toggle("show");
+    setTimeout(function () {
+        choice_menu.classList.remove("show");
+    }, 10000);
+};
 
 
 
